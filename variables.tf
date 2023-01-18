07b0480b26f49561e502f5d8538789b7be2c8f95
viktor.abudihin <viktor.abudihin@realize.dev>
2023-01-18 13:40:56 +0300

    Edit Readme and nginx role

--- variables.tf
+++ variables.tf
@@ -3,15 +3,15 @@ variable "zone" {
   default = "ru-central1-a"
 }
 
-# variable "cloud_id" {
-#   type    = string
-#   default = ""
-# }
+variable "cloud_id" {
+  type    = string
+  default = "b1goe3dambm08tdcqvcb"
+}
 
-# variable "folder_id" {
-#   type    = string
-#   default = ""
-# }
+variable "folder_id" {
+  type    = string
+  default = "b1g8orq0tmhvaddbqf99"
+}
 
 variable "image_id" {
   type = string
@@ -19,7 +19,7 @@ variable "image_id" {
   default = "fd816jiq3n13qtli6fh3" #centos 8 stream
 }
 
-# variable "yc_token" {
-#   type    = string
-#   default = ""
-# }
+variable "yc_token" {
+  type    = string
+  default = "t1.9euelZrOx5WdjIzJkY-Sxsebi43HlO3rnpWans6UkJTKnpuOjYvGjI6NyZLl8_dQQ2Fh-e8sIEhu_d3z9xByXmH57ywgSG79zef1656VmpySxpSMz8-YzpTNmMaJmZKZ7_0.LEd8ktroD2-2Bngom0KLiSQFE_D8COFYxnPP2LK7STpqSInFlI5jDFS33IJkgzaPNmDG9el3-wqC2RnWkcIBAA"
+}
